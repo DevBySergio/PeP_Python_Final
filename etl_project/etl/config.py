fc00bbe2845c973ee4813edc306e5c5075f1a79d
Sergio del Cerro <DevBySergio@gmail.com>
2026-05-05 12:25:13 +0200

    Sergio: Config - Revisar credenciales, hay que limpiar y reiniciar proyecto para puesta a punto

--- etl_project/etl/config.py
+++ etl_project/etl/config.py
@@ -39,3 +39,27 @@ BUENAS PRÁCTICAS:
 - Leer variables desde entorno (.env)
 - Definir valores por defecto seguros
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+INPUT_PATH = os.path.join(BASE_DIR, "data", "input")
+OUTPUT_PATH = os.path.join(BASE_DIR, "data", "output")
+LOG_PATH = os.path.join(BASE_DIR, "logs")
+
+CLIENTES_PREFIX = "Clientes"
+TARJETAS_PREFIX = "Tarjetas"
+FILE_EXTENSION = ".csv"
+
+DB_HOST = os.getenv("POSTGRES_HOST", "postgres")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+DB_NAME = os.getenv("POSTGRES_DB", "etl_db") 
+DB_USER = os.getenv("POSTGRES_USER", "etl_user")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "etl_pass")
+
+HASH_SALT = os.getenv("HASH_SALT", "default_salt_change_me")
+
+FLAG_TRUE = "Y"
+FLAG_FALSE = "N"
